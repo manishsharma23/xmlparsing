be66d4dc0ef9929caf033d39fb2e034bcae96d77
manish sharma <manish.sharma23@gmail.com>
2019-12-30 23:26:27 +0530

    xml parsing

---
+++ src/domparsingdemo/User.java
@@ -0,0 +1,46 @@
+package domparsingdemo;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class User {
+
+	
+	private int id;
+    private String firstName;
+    public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	private String lastName;
+    private int age;
+    private String gender; 
+}
